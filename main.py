57899937f0cd3c1608ca3a7662083c315377f5bb
Carlos <carlosmateus7720@gmail.com>
2024-07-07 13:20:08 -0300

    adicionando banco de dados

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from tkinter import *
+import sqlite3
 import os
+global resultado
+#criando um banco de dados
+def CriarDB():
+    conn = sqlite3.connect('banco_de_dados.sql')
+    cursor = conn.cursor()
+    print("db feito")
+
+
+    cursor.execute("""
+                CREATE TABLE IF NOT EXISTS Historico (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                operacao VARCHAR (255) NOT NULL,
+                resultado_final VARCHAR(255) NOT NULL
+                )
+                """)
+    conn.commit()
+    conn.close()
+CriarDB()
+pass
+
+
+
+
+
 #op de limpar o campo de expressao
 def clear():
     global expression
     expression = ""
     equacao.set("")
+
 #adicionando ao campo de expressao
 def adicionar(num):
     global expression
@@ -12,36 +38,37 @@ def adicionar(num):
     equacao.set(expression)
 #op de calcular
 
-def calcular():
+def calcular_operacao(operacao):
     try:
         global expression
-        resultado = str(eval(expression))  
-        equacao.set(resultado)
-        expression = resultado   
+        if operacao == "dividir":
+            expression += " / "
+        elif operacao == "multiplicar":
+            expression += " * "
+        else:
+            resultado = str(eval(expression))  
+            equacao.set(resultado)
+            expression = resultado 
+            adicionar_o_db(expression, resultado)
     except:
         equacao.set("Erro")
         expression = "" 
-#op de divisao
-def dividir():
-    try:
-        global expression
-        resultado = str(eval(expression))  
-        equacao.set(resultado)
-        expression = resultado   
-    except:
-        equacao.set("Erro")
-        expression = "" 
-#op de multiplicacao
+        
+        
+        
+def adicionar_o_db(expression, resultado):
+    if os.path.exists('banco_de_dados.sql'):
+        conn = sqlite3.connect('banco_de_dados.sql')
+        cursor = conn.cursor()
+        query = """
+            INSERT INTO Historico (operacao, resultado_final) VALUES (?,?)
+        """
+        cursor.execute(query, (int(expression), resultado))
+        conn.commit()
+        conn.close()
 
-def multiplicar():
-    try:
-        global expression
-        resultado = str(eval(expression))  
-        equacao.set(resultado)
-        expression = resultado   
-    except:
-        equacao.set("Erro")
-        expression = "" 
+
+
 
 if __name__ == "__main__":
     janela = Tk()
@@ -98,16 +125,25 @@ if __name__ == "__main__":
     menos.grid(row=2, column=4)
     
     #calcular
-    igual = Button(janela, text=' = ', fg='black', bg='white', command=calcular, height=1, width=7)
+    igual = Button(janela, text=' = ', fg='black', bg='white', command=lambda: calcular_operacao("igual"), height=1, width=7)
     igual.grid(row=1, column=4,)
     
+    
     #dividir
-    divide = Button(janela, text='/', fg='black', bg='white', command=lambda: press('/'), height=1, width=7)
-    divide.grid(row=3, column=4)
+    dividir_button = Button(janela, text="/", command=lambda: calcular_operacao("dividir"),height=1, width=7)
+    dividir_button.grid(row=3, column=4)
     
     #multiplicar
-    multiplica = Button(janela, text='*', fg='black', bg='white', command=lambda: press('*'), height=1, width=7)
-    multiplica.grid(row=3, column=5)
+    multiplicar_button = Button(janela, text="*", command=lambda: calcular_operacao("multiplicar"),height=1, width=7)
+    multiplicar_button.grid(row=3, column=5)
+    
+    
+    
+        
+
+        
+        
+        
     
     
     
